27229000f44ce6d6b339c7131aa8648e5ca84847
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-12-02 22:39:20 +0530

    beta V1.6

--- assets/js/products.js
+++ assets/js/products.js
@@ -47,13 +47,15 @@ function productHTML() {
 
     document.getElementById('productList').innerHTML += '<div id="'+ productArray[i].key +
     '" class="project item col-md-6 col-xl-4 '+ productArray[i].category +'">'+
-      '<figure class="lift rounded mb-6"><a href="#"> <img src="'+ productArray[i].imageURL +'" alt=""/></a></figure>'+
+      '<figure class="lift rounded mb-6"><a href="https://wa.me/971508337134?text=Hi%2C%20I%20want%20to%20place%20an%20order('+
+      productArray[i].name +')"> <img src="'+ productArray[i].imageURL +'" alt=""/></a></figure>'+
       '<div class="project-details justify-content-center custom-grid">'+
         '<div class="post-header">'+
           '<h2 class="post-title h3">'+ productArray[i].name +'</h2>'+
           '<div class="post-category text-line mb-3 text-purple">'+ productArray[i].category +'</div>'+
         '</div>'+
-        '<a href="#" class="btn btn-primary">Order Now</a>'+
+        '<a href="https://wa.me/971508337134?text=Hi%2C%20I%20want%20to%20place%20an%20order('+ productArray[i].name +
+        ')" class="btn btn-primary">Order Now</a>'+
       '</div>'+
     '</div>'
   }
